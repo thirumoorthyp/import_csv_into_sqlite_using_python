7fa412b367792a67090ce59f29353a72b99f3fc1
Thirumoorthy P <thirumoorthy.p@payoda.com>
2023-06-28 12:24:31 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# import_csv_into_sqlite_using_python
+# ***This Python code is used to import data from CSV file into sqlite database.***
+```shell
+python3 import_csv_into_sqlite_using_python.py
+```
+
---
+++ import_csv_into_sqlite_using_python.py
@@ -0,0 +1,25 @@
+# Import csv, sqlite3 and time modules
+import csv, sqlite3 , time
+# Import time module
+ 
+# record start time
+start = time.time()
+ 
+con = sqlite3.connect("./sqlite_employees.db") # change to 'sqlite:///your_filename.db'
+cur = con.cursor()
+
+with open('employees.csv','r') as fin: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [(i['emp_id'],i['emp_name'], i['dob'], i['role'], i['dept']) for i in dr]
+
+cur.executemany("INSERT INTO employee_details (emp_id, emp_name, emp_dob, emp_role, emp_dept) VALUES (?,?,?,?,?);", to_db)
+con.commit()
+con.close()
+# print the difference between start
+# and end time in milli. secs
+# record end time
+
+end = time.time()
+print("The time of execution of python program is :",
+      (end-start) * 10**3, "ms")
