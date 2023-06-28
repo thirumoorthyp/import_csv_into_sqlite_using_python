83b1462da6a0b1d82f9a309a2dce08462515c693
Thirumoorthy Panneerselvam <thirumoorthy.p@ptpll797.payoda.net>
2023-06-28 12:07:46 +0530

    initial commit

---
+++ import_csv_into_sqlite_using_python.py
@@ -0,0 +1,30 @@
+import csv, sqlite3
+# Import time module
+import time
+ 
+# record start time
+start = time.time()
+ 
+# define a sample code segment
+a = 0
+for i in range(1000):
+    a += (i**100)
+ 
+# record end time
+end = time.time()
+ 
+con = sqlite3.connect("./sqlite_employees.db") # change to 'sqlite:///your_filename.db'
+cur = con.cursor()
+
+with open('employees.csv','r') as fin: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [(i['emp_id'],i['emp_name'], i['dob'], i['role'], i['dept']) for i in dr]
+
+cur.executemany("INSERT INTO employee_details (emp_id, emp_name, emp_dob, emp_role, emp_dept) VALUES (?,?,?,?,?);", to_db)
+con.commit()
+con.close()
+# print the difference between start
+# and end time in milli. secs
+print("The time of execution of python program is :",
+      (end-start) * 10**3, "ms")
